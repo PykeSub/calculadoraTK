71ca5c5a19240e6897fc91c0c06ee849992f9207
PykeSub <gastonalvarez0023@gmail.com>
2024-06-18 18:07:21 -0400

    V2
    Se le agregó el tipo de botones y el tamaño.
    
    * font = ('Arial', 24)

--- Calculadora.py
+++ Calculadora.py
@@ -45,36 +45,36 @@ def calcular():
         
 # Botones Numericos
 
-Button(root, text = '1', command = lambda: obtener_numeros(1)).grid(row = 2, column = 0, sticky= W+E)
-Button(root, text = '2', command = lambda: obtener_numeros(2)).grid(row = 2, column = 1, sticky= W+E)
-Button(root, text = '3', command = lambda: obtener_numeros(3)).grid(row = 2, column = 2, sticky= W+E)
+Button(root, text = '1', font = ('Arial', 24), command = lambda: obtener_numeros(1)).grid(row = 2, column = 0, sticky= W+E)
+Button(root, text = '2', font = ('Arial', 24), command = lambda: obtener_numeros(2)).grid(row = 2, column = 1, sticky= W+E)
+Button(root, text = '3', font = ('Arial', 24), command = lambda: obtener_numeros(3)).grid(row = 2, column = 2, sticky= W+E)
 
-Button(root, text = '4', command = lambda: obtener_numeros(4)).grid(row = 3, column = 0, sticky= W+E)
-Button(root, text = '5', command = lambda: obtener_numeros(5)).grid(row = 3, column = 1, sticky= W+E)
-Button(root, text = '6', command = lambda: obtener_numeros(6)).grid(row = 3, column = 2, sticky= W+E)
+Button(root, text = '4', font = ('Arial', 24), command = lambda: obtener_numeros(4)).grid(row = 3, column = 0, sticky= W+E)
+Button(root, text = '5', font = ('Arial', 24), command = lambda: obtener_numeros(5)).grid(row = 3, column = 1, sticky= W+E)
+Button(root, text = '6', font = ('Arial', 24), command = lambda: obtener_numeros(6)).grid(row = 3, column = 2, sticky= W+E)
 
-Button(root, text = '7', command = lambda: obtener_numeros(7)).grid(row = 4, column = 0, sticky= W+E)
-Button(root, text = '8', command = lambda: obtener_numeros(8)).grid(row = 4, column = 1, sticky= W+E)
-Button(root, text = '9', command = lambda: obtener_numeros(9)).grid(row = 4, column = 2, sticky= W+E)
+Button(root, text = '7', font = ('Arial', 24), command = lambda: obtener_numeros(7)).grid(row = 4, column = 0, sticky= W+E)
+Button(root, text = '8', font = ('Arial', 24), command = lambda: obtener_numeros(8)).grid(row = 4, column = 1, sticky= W+E)
+Button(root, text = '9', font = ('Arial', 24), command = lambda: obtener_numeros(9)).grid(row = 4, column = 2, sticky= W+E)
 
 # Botones PART2
 
-Button(root, text = 'AC', command = lambda: clear_display()).grid(row = 5, column = 0, sticky= W+E)
-Button(root, text = '0', command = lambda: obtener_numeros(0)).grid(row = 5, column = 1, sticky= W+E)
-Button(root, text = '%', command = lambda: obtener_operacion('%')).grid(row = 5, column = 2, sticky= W+E)
+Button(root, text = 'AC', font = ('Arial', 24), command = lambda: clear_display()).grid(row = 5, column = 0, sticky= W+E)
+Button(root, text = '0', font = ('Arial', 24), command = lambda: obtener_numeros(0)).grid(row = 5, column = 1, sticky= W+E)
+Button(root, text = '%', font = ('Arial', 24), command = lambda: obtener_operacion('%')).grid(row = 5, column = 2, sticky= W+E)
 
 # Botnoes de Operaciones
 
-Button(root, text = '+', command = lambda: obtener_operacion('+')).grid(row = 2, column = 3, sticky= W+E)
-Button(root, text = '-', command = lambda: obtener_operacion('-')).grid(row = 3, column = 3, sticky= W+E)
-Button(root, text = '*', command = lambda: obtener_operacion('*')).grid(row = 4, column = 3, sticky= W+E)
-Button(root, text = '/', command = lambda: obtener_operacion('/')).grid(row = 5, column = 3, sticky= W+E)
+Button(root, text = '+', font = ('Arial', 24), command = lambda: obtener_operacion('+')).grid(row = 2, column = 3, sticky= W+E)
+Button(root, text = '-', font = ('Arial', 24), command = lambda: obtener_operacion('-')).grid(row = 3, column = 3, sticky= W+E)
+Button(root, text = '*', font = ('Arial', 24), command = lambda: obtener_operacion('*')).grid(row = 4, column = 3, sticky= W+E)
+Button(root, text = '/', font = ('Arial', 24), command = lambda: obtener_operacion('/')).grid(row = 5, column = 3, sticky= W+E)
 
-Button(root, text = '🠔', command = lambda: undo()).grid(row = 2, column = 4, sticky= W+E, columnspan = 2)
-Button(root, text = 'EXP', command = lambda: obtener_operacion('**')).grid(row = 3, column = 4, sticky= W+E)
-Button(root, text = '^2', command = lambda: obtener_operacion('**2')).grid(row = 3, column = 5, sticky= W+E)
-Button(root, text = '(', command = lambda: obtener_operacion('()')).grid(row = 4, column = 4, sticky= W+E)
-Button(root, text = ')', command = lambda: obtener_operacion(')')).grid(row = 4, column = 5, sticky= W+E)
-Button(root, text = '=', command = lambda: calcular()).grid(row = 5, column = 4, sticky= W+E, columnspan = 2)
+Button(root, text = '🠔', font = ('Arial', 24), command = lambda: undo()).grid(row = 2, column = 4, sticky= W+E, columnspan = 2)
+Button(root, text = 'EXP', font = ('Arial', 24), command = lambda: obtener_operacion('**')).grid(row = 3, column = 4, sticky= W+E)
+Button(root, text = '^2', font = ('Arial', 24), command = lambda: obtener_operacion('**2')).grid(row = 3, column = 5, sticky= W+E)
+Button(root, text = '(', font = ('Arial', 24), command = lambda: obtener_operacion('()')).grid(row = 4, column = 4, sticky= W+E)
+Button(root, text = ')', font = ('Arial', 24), command = lambda: obtener_operacion(')')).grid(row = 4, column = 5, sticky= W+E)
+Button(root, text = '=', font = ('Arial', 24), command = lambda: calcular()).grid(row = 5, column = 4, sticky= W+E, columnspan = 2)
 
 root.mainloop()
